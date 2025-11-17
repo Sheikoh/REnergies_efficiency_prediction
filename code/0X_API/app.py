b644578bfaa59e12402d524781acbca4dc918b9b
Sheikoh <john-lee.dubos@protonmail.com>
2025-11-17 12:03:00 +0100

    try out

--- code/0X_API/app.py
+++ code/0X_API/app.py
@@ -173,10 +173,10 @@ async def predict(predictionFeatures: dict[str, Union[str, float]]):
     data_employee = pd.DataFrame([predictionFeatures])
 
     # Log model from mlflow 
-    # logged_model = 'runs:/d81138d3368d4c048bafd9c7fefd70d5/model'
-    logged_model = "s3://renergies99-mlflow/4/d81138d3368d4c048bafd9c7fefd70d5/artifacts/model"
+    logged_model = 'runs:/d81138d3368d4c048bafd9c7fefd70d5/model'
+    # logged_model = "s3://renergies99-mlflow/4/d81138d3368d4c048bafd9c7fefd70d5/artifacts/model"
     print(logged_model)
-    
+
 
     # # Load model as a PyFuncModel.
     loaded_model = mlflow.pyfunc.load_model(logged_model)
