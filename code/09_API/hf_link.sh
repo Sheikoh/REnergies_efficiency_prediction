b84bd772e034a5080674985a9932cc2b7f354c9e
Sheikoh <john-lee.dubos@protonmail.com>
2025-11-18 14:16:29 +0100

    update

--- code/09_API/hf_link.sh
+++ code/09_API/hf_link.sh
@@ -1,6 +1,7 @@
 #This file gives the console commands to link the repo to the hugging face space
 
 git remote add hf_REnergy https://huggingface.co/spaces/REnergies99/API_REnergy
+# to be modified
 
 #initiate the repo hugging_face. 
 # BEWARE: this deletes the Mandatory README.md created by Hugging face, which you will need to recreate.
