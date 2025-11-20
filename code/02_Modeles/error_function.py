6f568d495bec4d91f5786acfde70604eb476f2ac
sandrinehardy <hardy.sand.sh@gmail.com>
2025-11-20 15:59:16 +0100

    training code in notebook

--- code/02_Modeles/error_function.py
+++ code/02_Modeles/error_function.py
@@ -1,8 +1,9 @@
-def get_std(x, table):
+def get_std(x, table_json):
+   table = pd.Dataframe(table_json)
    if x <= min(table['min']):
       std = table['std'][0]
    elif x > max(table['max']):
       std = table['std'].iloc[-1]
    else:
       std = table[(x > table['min']) & (x <= table['max'])]['std']
-   return std
+   return x, std
