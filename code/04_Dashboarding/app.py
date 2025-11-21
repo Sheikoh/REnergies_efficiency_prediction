48ce70217dc3ffb1ed15b373939dd022b508e46e
Jiro99 <nouane_v@hotmail.com>
2025-11-21 13:12:03 +0100

    Fix predict call in dashboard

--- code/04_Dashboarding/app.py
+++ code/04_Dashboarding/app.py
@@ -129,10 +129,10 @@ def load_predictions_file() -> pd.DataFrame:
     return df_pred[["Date", COL_TCH, "type"]]
 
 def call_predict():
-    data = requests.post("https://renergies99-api-renergy.hf.space/prep_data", [
+    data = requests.post("https://renergies99-api-renergy.hf.space/prep_data", json={[
         'https://renergies99-bucket.s3.eu-west-3.amazonaws.com/public/solar/predi_data.csv',
         'https://renergies99-bucket.s3.eu-west-3.amazonaws.com/public/openweathermap/openweathermap_forecasts.csv'
-        ])
+        ]})
     json_data = data.content.to_json()
 
     response = requests.post("https://renergies99-api-renergy.hf.space/predict", json=json_data)
